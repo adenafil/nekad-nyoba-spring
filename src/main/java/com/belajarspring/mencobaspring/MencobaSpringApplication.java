e5764702bf4389f4bf10f7815b50d8a87e4ade43
adenafil <nafilie9@gmail.com>
2023-12-07 23:11:39 +0700

    first commit

---
+++ src/main/java/com/belajarspring/mencobaspring/MencobaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.belajarspring.mencobaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MencobaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MencobaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belajarspring/mencobaspring/MencobaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajarspring.mencobaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MencobaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
